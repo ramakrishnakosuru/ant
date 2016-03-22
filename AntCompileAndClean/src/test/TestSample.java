6c58ea9a7e8523795267716df6bfd3814c192026
Ramakrishna <ramakrishna.kosuru@yahoo.com>
2016-03-22 17:43:07 +0530

    ram commit

--- AntCompileAndClean/src/test/TestSample.java
+++ AntCompileAndClean/src/test/TestSample.java
@@ -5,7 +5,8 @@ import sample.Sample;
 public class TestSample {
     public static void main(String[] args) {
 	Sample sample = new Sample();
-	int result = sample.add(1, 2);
+	int result = sample.add(5,6);
+	System.out.println("ram  " );
 	System.out.println("result = " + result);
     }
 }
